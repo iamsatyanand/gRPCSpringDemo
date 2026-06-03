4381075329c0dd2483a56b2eb7d97d20a784823d
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-06-03 23:00:47 +0530

    grpc service implemented

--- gRPCServer/src/main/java/com/satyanand/gRPCServer/services/SampleServiceImpl.java
+++ gRPCServer/src/main/java/com/satyanand/gRPCServer/services/SampleServiceImpl.java
@@ -1,6 +1,20 @@
 package com.satyanand.gRPCServer.services;
 
+import com.satyanand.gRPCServer.SampleRequest;
+import com.satyanand.gRPCServer.SampleResponse;
 import com.satyanand.gRPCServer.SampleServiceGrpc;
+import io.grpc.stub.StreamObserver;
 
 public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
+
+    @Override
+    public void SaySampleHello(SampleRequest request, StreamObserver<SampleResponse> responseObserver){
+        String name = request.getName();
+        String message = "Hello, "+ name + "; Welcome to the sample gRPC service";
+
+        SampleResponse response = SampleResponse.newBuilder().setMessage(message).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
