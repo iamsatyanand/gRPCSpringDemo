b6ec67f14e194f5e1dcbd698b22b226a40fd179b
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-06-03 22:48:18 +0530

    added plugins, dependencies, compiler and required configuration for protcol buffer and grpc in build.gradle of gRPC server

---
+++ gRPCServer/src/main/java/com/satyanand/gRPCServer/services/SampleServiceImpl.java
@@ -0,0 +1,6 @@
+package com.satyanand.gRPCServer.services;
+
+import com.satyanand.gRPCServer.SampleServiceGrpc;
+
+public class SampleServiceImpl extends SampleServiceGrpc.SampleServiceImplBase {
+}
